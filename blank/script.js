dfda9c7e941624eb2257b60bc50d37f90346f107
pack3t1etha1 <101640386+pack3t1etha1@users.noreply.github.com>
2022-03-31 12:11:44 +0100

    Delete script.js

--- blank/script.js
+++
@@ -1,6 +0,0 @@
-/* inspired by this codepen:
-https://codepen.io/stefanweck/pen/VmOMoE
-
-widths and spacing are based on viewport width
-
-resize the viewport to change how close each line is or how tall the "wave" is*/
