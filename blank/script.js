2fd510e2f65eb6fd8dd962243fa25cdc4c7e48d5
pack3t1etha1 <101640386+pack3t1etha1@users.noreply.github.com>
2022-03-31 12:09:39 +0100

    Add files via upload

---
+++ blank/script.js
@@ -0,0 +1,6 @@
+/* inspired by this codepen:
+https://codepen.io/stefanweck/pen/VmOMoE
+
+widths and spacing are based on viewport width
+
+resize the viewport to change how close each line is or how tall the "wave" is*/
